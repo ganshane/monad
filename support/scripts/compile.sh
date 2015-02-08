5788064b13dc516abc69d5602f23ce17e3c41f69
Jun Tsai <jcai@ganshane.com>
2015-02-08 22:44:33 +0800

    #14 尝试增加mingw的编译配置

---
+++ support/scripts/compile.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+if [ -z $CROSSPREFIX ]; then
+  apt-get install -qqy mingw-w64 g++-mingw-w64 swig
+  cd monad-c
+  LIB_DIR=$(pwd)/_tmp
+  mkdir -p $LIB_DIR
+  mkdir -p ${LIB_DIR}/mingw
+  mkdir -p ${LIB_DIR}/${ARCH}
+
+  #compile leveldb
+  cd $LIB_DIR
+  git clone --depth=1 --branch=bitcoin-fork https://github.com/bitcoin/leveldb.git
+  cd leveldb
+  CC=/usr/bin/${CROSSPREFIX}gcc CXX=${CROSSPREFIX}g++ TARGET_OS=OS_WINDOWS_CROSSCOMPILE make clean all
+  cp libleveldb.a ${LIB_DIR}/${ARCH}
+  cp -rp include/leveldb ${LIB_DIR}/mingw/
+
+  #compile 
+  cd ${LIB_DIR}/..
+  mkdir build-$ARCH
+  cd build-$ARCH
+  LDFLAGS="-L/${LIB_DIR}/${ARCH}" CXXFLAGS="-I${LIB_DIR}/mingw"   \
+    cmake -DCMAKE_TOOLCHAIN_FILE=${LIB_DIR}/../../support/docker/compile-mingw/Toolchain-cross-mingw32-linux.cmake \
+    -DHOST=$HOST  -DCMAKE_BUILD_TYPE=Release -DARCH=$ARCH \
+    -DSWIG_DIR=/usr/share/swig2.0 \
+    -DJAVA_AWT_LIBRARY=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/libjawt.so \
+    -DJAVA_INCLUDE_PATH=/usr/lib/jvm/java-7-openjdk-amd64/include \
+    -DJAVA_AWT_INCLUDE_PATH=/usr/lib/jvm/java-7-openjdk-amd64/include \
+    -DJAVA_INCLUDE_PATH2=/usr/lib/jvm/java-7-openjdk-amd64/include/linux \
+    -DJAVA_JVM_LIBRARY=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/libjava.so  ..
+  make
+else
+  cd monad-c && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make
+fi
