b439b80bfff29b6e438b148cca51f01ea6d9a4b2
Jun Tsai <jcai@ganshane.com>
2015-02-08 20:08:42 +0800

    FIXES #12 修正在MINGW下编译问题

--- support/docker/compile-mingw/compile.sh
+++ support/docker/compile-mingw/compile.sh
@@ -12,7 +12,7 @@ trap save_and_shutdown EXIT
 
 git clone --branch=develop --depth=1 https://github.com/ganshane/monad.git .
 #export VERSION=`cat pom.xml|grep -e '<version>\(.*\)</version>'|head -n 1|sed  's/^.*<version>\(.*\)<\/version>/\1/g'`
-export VERSION=`git log -n1 --pretty=format:%at | awk '{print strftime("%y%m%d%H%M",$1)}'`
+#export VERSION=`git log -n1 --pretty=format:%at | awk '{print strftime("%y%m%d%H%M",$1)}'`
 export VERSION=`git log -n1 --pretty=format:%ai|cut -c1-10`
 
 cd monad-c
@@ -23,7 +23,16 @@ export ARCH=w32
 export CROSSPREFIX=i686-w64-mingw32-
 mkdir build-$ARCH
 cd build-$ARCH
-cmake -DNIRVANA_SOURCE_VERSION=$VERSION -DARCH=$ARCH -DHOST=$HOST -DCMAKE_TOOLCHAIN_FILE=/build/Toolchain-cross-mingw32-linux.cmake   -DCMAKE_BUILD_TYPE=Release ..
+LDFLAGS="-L/build/${ARCH}" CXXFLAGS="-I/build/mingw"   \
+  cmake -DCMAKE_TOOLCHAIN_FILE=/build/Toolchain-cross-mingw32-linux.cmake \
+  -DHOST=$HOST  -DCMAKE_BUILD_TYPE=Release -DARCH=$ARCH \
+  -DSWIG_DIR=/usr/share/swig2.0 \
+  -DJAVA_AWT_LIBRARY=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/libjawt.so \
+  -DJAVA_INCLUDE_PATH=/usr/lib/jvm/java-7-openjdk-amd64/include \
+  -DJAVA_AWT_INCLUDE_PATH=/usr/lib/jvm/java-7-openjdk-amd64/include \
+  -DJAVA_INCLUDE_PATH2=/usr/lib/jvm/java-7-openjdk-amd64/include/linux \
+  -DJAVA_JVM_LIBRARY=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/libjava.so  ..
+
 make
 cd -
 
@@ -35,7 +44,15 @@ export ARCH=w64
 export CROSSPREFIX=x86_64-w64-mingw32-
 mkdir build-$ARCH
 cd build-$ARCH
-cmake -DNIRVANA_SOURCE_VERSION=$VERSION  -DARCH=$ARCH -DHOST=$HOST -DCMAKE_TOOLCHAIN_FILE=/build/Toolchain-cross-mingw32-linux.cmake   -DCMAKE_BUILD_TYPE=Release ..
+LDFLAGS="-L/build/${ARCH}" CXXFLAGS="-I/build/mingw"   \
+  cmake -DCMAKE_TOOLCHAIN_FILE=/build/Toolchain-cross-mingw32-linux.cmake \
+  -DHOST=$HOST  -DCMAKE_BUILD_TYPE=Release -DARCH=$ARCH \
+  -DSWIG_DIR=/usr/share/swig2.0 \
+  -DJAVA_AWT_LIBRARY=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/libjawt.so \
+  -DJAVA_INCLUDE_PATH=/usr/lib/jvm/java-7-openjdk-amd64/include \
+  -DJAVA_AWT_INCLUDE_PATH=/usr/lib/jvm/java-7-openjdk-amd64/include \
+  -DJAVA_INCLUDE_PATH2=/usr/lib/jvm/java-7-openjdk-amd64/include/linux \
+  -DJAVA_JVM_LIBRARY=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/libjava.so  ..
 make
 cd -
 
