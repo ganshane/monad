fa6b17477991aa517f170c12243aec5d426f5d30
Jun Tsai <jcai@ganshane.com>
2015-02-09 12:47:26 +0800

    FIXES #10 调整编译环境，支持maven编译

--- support/scripts/travis.sh
+++ support/scripts/travis.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+ROOT_DIR=$(pwd)
+#create jni directory
+mkdir  -p ${ROOT_DIR}/monad-jni/src/main/java/monad/jni/services/gen
+
 if [ $CROSSPREFIX ]; then
   sudo apt-get -yy purge mingw32 mingw32-binutils
   sudo apt-get install -yy mingw-w64 g++-mingw-w64 swig
@@ -33,7 +37,17 @@ if [ $CROSSPREFIX ]; then
     -DJAVA_INCLUDE_PATH2=/usr/lib/jvm/java-7-openjdk-amd64/include/linux \
     -DJAVA_JVM_LIBRARY=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/libjava.so  ..
   make
+
 else
   sudo apt-get -yy install libleveldb-dev swig libsnappy-dev
-  cd monad-c && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make
+  cd monad-c
+  mkdir build 
+  cd build 
+  cmake -DCMAKE_BUILD_TYPE=Release .. 
+  make
 fi
+
+#compile jni files
+cp src/javaapi/*.java ${ROOT_DIR}/monad-jni/src/main/java/monad/jni/services/gen/
+
+mvn clean test
