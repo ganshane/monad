d1648fb4e167f7faee98657e34e4a67adbf3dfda
Jun Tsai <jcai@ganshane.com>
2015-02-28 20:46:49 +0800

    调整travis为统一编译，同时产生win和linux下的文件

--- support/scripts/travis.sh
+++ support/scripts/travis.sh
@@ -2,30 +2,32 @@
 
 ROOT_DIR=$(pwd)
 #create jni directory
-mkdir  -p ${ROOT_DIR}/monad-jni/src/main/java/monad/jni/services/gen
+mkdir -p ${ROOT_DIR}/monad-jni/src/main/java/monad/jni/services/gen
 mkdir -p ${ROOT_DIR}/support/dll
 
-if [ $CROSSPREFIX ]; then
-  sudo apt-get -yy purge mingw32 mingw32-binutils
-  sudo apt-get install -yy mingw-w64 g++-mingw-w64 swig
+sudo apt-get -yy purge mingw32 mingw32-binutils
+sudo apt-get install -yy mingw-w64 g++-mingw-w64 swig
+
+LIB_DIR=$(pwd)/_tmp
+mkdir -p $LIB_DIR
+mkdir -p ${LIB_DIR}/mingw
+#compile leveldb
+cd $LIB_DIR
+git clone --depth=1 --branch=bitcoin-fork https://github.com/bitcoin/leveldb.git
+
+function compile_mingw(){
 #  gcc-mingw-w64 g++-mingw-w64-x86-64 g++-mingw-w64-i686 gcc-mingw-w64-i686 gcc-mingw-w64-x86-64 \
 #  binutils-mingw-w64-i686 binutils-mingw-w64-x86-64
-  cd monad-c
-  LIB_DIR=$(pwd)/_tmp
-  mkdir -p $LIB_DIR
-  mkdir -p ${LIB_DIR}/mingw
   mkdir -p ${LIB_DIR}/${ARCH}
 
   #compile leveldb
-  cd $LIB_DIR
-  git clone --depth=1 --branch=bitcoin-fork https://github.com/bitcoin/leveldb.git
-  cd leveldb
+  cd $LIB_DIR/leveldb
   CC=/usr/bin/${CROSSPREFIX}gcc CXX=${CROSSPREFIX}g++ AR=${CROSSPREFIX}ar TARGET_OS=OS_WINDOWS_CROSSCOMPILE make clean all
   cp libleveldb.a ${LIB_DIR}/${ARCH}
   cp -rp include/leveldb ${LIB_DIR}/mingw/
 
   #compile 
-  cd ${LIB_DIR}/..
+  cd ${ROOT_DIR}/monad-c
   mkdir build-$ARCH
   cd build-$ARCH
   LDFLAGS="-L/${LIB_DIR}/${ARCH}" CXXFLAGS="-I${LIB_DIR}/mingw"   \
@@ -38,16 +40,18 @@ if [ $CROSSPREFIX ]; then
     -DJAVA_INCLUDE_PATH2=/usr/lib/jvm/java-7-openjdk-amd64/include/linux \
     -DJAVA_JVM_LIBRARY=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/libjava.so  ..
   make
-  #windows not execute maven test case
-  exit $?
-else
-  sudo apt-get -yy install libleveldb-dev swig libsnappy-dev
-  cd monad-c
-  mkdir build
-  cd build
-  cmake -DCMAKE_BUILD_TYPE=Release ..
-  make
-fi
+  cp src/libmonad4j.dll ${ROOT_DIR}/support/dll/monad4j-${ARCH}.dll
+}
+HOST=x86_64-w64-mingw32 ARCH=w64 CROSSPREFIX=x86_64-w64-mingw32-  compile_mingw
+HOST=i686-w64-mingw32 ARCH=w32 CROSSPREFIX=i686-w64-mingw32- compile_mingw
+
+#compile linux version
+sudo apt-get -yy install libleveldb-dev swig libsnappy-dev
+cd monad-c
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
 
 #compile jni files
 cp src/javaapi/*.java ${ROOT_DIR}/monad-jni/src/main/java/monad/jni/services/gen/
