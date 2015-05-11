be2b06f2c9586c66c1d17bcb66b646abb242bbd8
Jun Tsai <jcai@ganshane.com>
2015-05-11 11:28:35 +0800

    添加前缀和等级输出

--- monad-core/src/main/scala/monad/core/services/GlobalLoggerConfigurationSupport.scala
+++ monad-core/src/main/scala/monad/core/services/GlobalLoggerConfigurationSupport.scala
@@ -31,7 +31,7 @@ trait GlobalLoggerConfigurationSupport {
       properties.put("log4j.rootCategory", "error,R")
       properties.put("log4j.appender.R", "org.apache.log4j.RollingFileAppender")
       properties.put("log4j.appender.R.layout", "org.apache.log4j.PatternLayout")
-      properties.put("log4j.appender.R.layout.ConversionPattern", "[" + prefix + "] %d{MM-dd HH:mm:ss} %m%n")
+      properties.put("log4j.appender.R.layout.ConversionPattern", "[" + prefix + "] %d{MM-dd HH:mm:ss} [%p] %m%n")
       properties.put("log4j.appender.R.File", logFile)
       properties.put("log4j.appender.R.MaxFileSize", "10000KB")
       properties.put("log4j.appender.R.MaxBackupIndex", "10")
