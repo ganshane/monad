1d9d91c46eeafe356786c0ac65cd34a9ed543ecb
Jun Tsai <jcai@ganshane.com>
2015-09-18 12:07:05 +0800

    FIXES #113 增加对名字分词的分词器

---
+++ monad-mmseg/src/test/java/monad/mmseg/analysis/NameAnalyzerTest.java
@@ -0,0 +1,28 @@
+package monad.mmseg.analysis;
+
+import junit.framework.Assert;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author <a href="mailto:jcai@ganshane.com">Jun Tsai</a>
+ * @since 2015-09-18
+ */
+public class NameAnalyzerTest {
+    @Test
+    public void test_analyzer() throws IOException {
+        NameAnalyzer analyzer = new NameAnalyzer();
+        TokenStream tokenStream = analyzer.tokenStream("", "李劲松");
+        tokenStream.reset();
+        CharTermAttribute term = tokenStream.getAttribute(CharTermAttribute.class);
+        StringBuilder sb = new StringBuilder();
+        while(tokenStream.incrementToken()){
+            sb.append(term).append("|");
+        }
+
+        Assert.assertEquals("李|劲|松|",sb.toString());
+    }
+}
