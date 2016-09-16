6da92d614d7c381d83c4b05aa08c74ec72b7d6f9
Jun Tsai <jcai@ganshane.com>
2016-09-16 14:53:25 +0800

    upgrade analytics4j compile flags

---
+++ support/docker/compile-mingw-w64/build.sh
@@ -0,0 +1,1 @@
+docker build --rm -t compiler-monad-mingw-w64 .
--- support/docker/compile-mingw-w64/compile.sh
+++ support/docker/compile-mingw-w64/compile.sh
@@ -4,14 +4,13 @@ set -e
 save_and_shutdown() {
   # save built for host result
   # force clean shutdown
-  #halt -f
-  echo "exit"
+  halt -f
 }
 
 # make sure we shut down cleanly
 trap save_and_shutdown EXIT
 
-git clone --branch=monad-6 --depth=1 https://github.com/ganshane/monad.git .
+git clone --branch=develop --depth=1 https://github.com/ganshane/monad.git .
 #export VERSION=`cat pom.xml|grep -e '<version>\(.*\)</version>'|head -n 1|sed  's/^.*<version>\(.*\)<\/version>/\1/g'`
 #export VERSION=`git log -n1 --pretty=format:%at | awk '{print strftime("%y%m%d%H%M",$1)}'`
 export VERSION=`git log -n1 --pretty=format:%ai|cut -c1-10`
@@ -23,7 +22,7 @@ ROOT=$(pwd)
 export HOST=i686-w64-mingw32
 export ARCH=w32
 export CROSSPREFIX=i686-w64-mingw32-
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-i386
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386
 cd $ROOT/monad-c
 mkdir build-$ARCH
 cd build-$ARCH
@@ -44,7 +43,6 @@ cd build-$ARCH
 LDFLAGS="-L/build/${ARCH}" CXXFLAGS="-I/build/mingw"   \
   cmake -DCMAKE_TOOLCHAIN_FILE=/build/Toolchain-cross-mingw32-linux.cmake \
   -DENABLE_JNI=on \
-  -DENABLE_SDK=on \
   -DHOST=$HOST  -DARCH=$ARCH \
   -DSWIG_DIR=/usr/share/swig2.0 \
   -DJAVA_AWT_LIBRARY=$JAVA_HOME/jre/lib/i386/libjawt.so \
@@ -60,7 +58,7 @@ cd -
 export HOST=x86_64-w64-mingw32
 export ARCH=w64
 export CROSSPREFIX=x86_64-w64-mingw32-
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 cd $ROOT/monad-c
 mkdir build-$ARCH
 cd build-$ARCH
@@ -82,7 +80,6 @@ cd build-$ARCH
 LDFLAGS="-L/build/${ARCH}" CXXFLAGS="-I/build/mingw"   \
   cmake -DCMAKE_TOOLCHAIN_FILE=/build/Toolchain-cross-mingw32-linux.cmake \
   -DENABLE_JNI=on \
-  -DENABLE_SDK=on \
   -DHOST=$HOST  -DARCH=$ARCH \
   -DSWIG_DIR=/usr/share/swig2.0 \
   -DJAVA_AWT_LIBRARY=$JAVA_HOME/jre/lib/amd64/libjawt.so \
@@ -102,12 +99,7 @@ cp build-w64/monad-jni-gen.tar.gz /dist
 
 cd $ROOT/monad-analytics
 cp build-w32/*.dll /dist/w32/analytics4j.dll
-cp build-w32/sdk/*.dll /dist/w32/libmonad.dll
-cp build-w32/sdk/*.exe /dist/w32/monad_demo.exe
-
 cp build-w64/*.dll /dist/w64/analytics4j.dll
-cp build-w64/sdk/*.dll /dist/w64/libmonad.dll
-cp build-w64/sdk/*.exe /dist/w64/monad_demo.exe
 
 
 echo "[hit enter key to exit] or run 'docker stop <container>'"
