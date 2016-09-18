8e057c5c1c3c89bb9df82894c77cfee701c9f5a4
Jun Tsai <jcai@ganshane.com>
2016-09-18 13:55:16 +0800

    修正windows下编译问题

--- monad-analytics/CRoaring-v0.2.1/src/containers/bitset.c
+++ monad-analytics/CRoaring-v0.2.1/src/containers/bitset.c
@@ -11,9 +11,9 @@
 #include <string.h>
 #ifdef __WIN32__
 int posix_memalign(void** mem,size_t alignment,size_t size){
-  *mem = _aligned_malloc(alignment, size);
- return 0;
- }
+  *mem = _aligned_malloc(size,alignment);
+  return 0;
+}
 #endif
 
 #include <roaring/portability.h>
