9bbab3439df8023140d08b2e37df80eba54fcd46
Jun Tsai <jcai@ganshane.com>
2016-12-13 14:33:34 +0800

    添加集合容器定义

--- monad-analytics/em/collection_info.h
+++ monad-analytics/em/collection_info.h
@@ -1,5 +1,8 @@
 #ifndef MONAD_COLLECTION_INFO_H_
 #define MONAD_COLLECTION_INFO_H_
+
+#include <map>
+
 #include "bit_set_wrapper.h"
 #include "roaring_bit_set_wrapper.h"
 #include "top_bit_set_wrapper.h"
@@ -32,6 +35,50 @@ namespace monad {
     RegionDoc** TopWrapper(int32_t n,int32_t& data_len);
     RegionTopDoc** TopWrapperTop(int32_t n,int32_t& data_len);
     int32_t BitCount();
+  };
+
+
+
+  template <typename K,typename COMPARATOR>
+  class CollectionContainer{
+  private:
+    //记录BitSetWrapper的容器
+    std::map<K,CollectionInfo*,COMPARATOR> _container;
+    typedef typename std::map<K,CollectionInfo*>::iterator CONTAINER_IT;
+  public:
+   /**
+   * 查找map中的wrapper对象
+   */
+   inline CollectionInfo* FindWrapper(const K& key){
+//    printf("find wrapper _container size:%d \n",map.size());
+      CONTAINER_IT it = _container.find(key);
+      if(it == _container.end()){
+        return NULL;
+      }else{
+        return it->second;
+      }
+    }
+    /**
+     * 清空某一个容器
+     */
+    inline void ClearContainer(){
+      for (CONTAINER_IT it=_container.begin(); it!=_container.end(); ++it){
+        delete it->second;
+      }
+      CONTAINER_IT it;
+      it = _container.begin();
+      _container.erase(it,_container.end());
+    }
+    /**
+     * 通过给定的key,删除某一个wrapper
+     */
+    inline void RemoveWrapper(const K& key){
+      CollectionInfo* wrapper = FindWrapper(key);
+      if(wrapper){
+        _container.erase(key);
+        delete wrapper;
+      }
+    }
   };
 }
 #endif //MONAD_COLLECTION_INFO_H_
